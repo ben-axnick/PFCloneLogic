076eb02963dce794e3bbd86c4e4e7010893b7113
baxnick@rakdos <baxnick@rakdos>
2012-06-25 20:25:01 +1000

    Added the TestDriver project that was left out of VCS

---
+++ PushFightDriver/TestDriver.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PushFightLogic
+{
+   class TestDriver
+   {
+      public static void Main(string[] args)
+      {
+         Dictionary<Coords, PieceType> p1Moves = new Dictionary<Coords, PieceType>(){
+				{new Coords(){x = 3, y = 1}, PieceType.ROUND},
+				{new Coords(){x = 1, y = 2}, PieceType.ROUND},
+				{new Coords(){x = 1, y = 3}, PieceType.SQUARE},
+				{new Coords(){x = 2, y = 3}, PieceType.SQUARE},
+				{new Coords(){x = 2, y = 4}, PieceType.SQUARE}
+			};
+
+         GameMaster master = new GameMaster();
+         master.Reset();
+
+         foreach (Coords coord in p1Moves.Keys)
+         {
+            master.Turn.Control().Place(p1Moves[coord], coord);
+         }
+
+         
+         AIEngine ai = new AIEngine(master);
+         ai.DebugFn = Debug;
+
+         ai.Act("Placement");
+         ai.Instructions.ForEach( i => i());
+         master.Turn.Control().Skip();
+         master.Turn.Control().Skip();
+
+         ai.Act("Movement");
+         ai.Instructions.ForEach(i => i());
+         Console.ReadLine();
+      }
+
+      static void Debug(object obj)
+      {
+         string output = obj as string;
+
+         if (output != null) Console.WriteLine(output);
+      }
+   }
+}
